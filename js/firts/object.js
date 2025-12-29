d2ba2d5006f98b22989d7e755c18343339b51138
Shailendra Rai <shailendrarai153@gmail.com>
2025-12-29 23:13:44 +0530

    promise

---
+++ js/firts/object.js
@@ -0,0 +1,33 @@
+ // promise 
+
+console.log("start")
+
+// const sub = new Promise((resolve , reject)=>{
+//   setTimeout(()=>{
+//      const result = true
+//      if (result) {
+//           resolve("hii promise fullfill")
+//      }else{
+//           reject("hii this resject promise")
+//      }
+//   })
+// })
+
+
+// sub.then((resolve)=>{
+//      console.log(resolve)
+// }).catch((error)=>{
+//    console.log(error)
+// })
+
+
+
+
+// console.log("end")
+
+
+
+
+
+
+ console.log("end")
